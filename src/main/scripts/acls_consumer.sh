7d1449eacf8782fbe8bdb01a249943a6d140826c
Eber H <eber.h@outlook.com>
2019-11-18 17:26:46 +0800

    acls bigdata

--- src/main/scripts/acls_consumer.sh
+++ src/main/scripts/acls_consumer.sh
@@ -1,18 +1,22 @@
-#!/bin/bash
-
 topic="test_console"
-group="test_console-consumer"
 if [ ! -z "$1" ]
 then
 topic="$1"
-group="\*"
-fi
-echo ${topic}
 
 /usr/hdp/current/kafka-broker/bin/kafka-acls.sh \
 --authorizer-properties zookeeper.connect=h11.bdiiot.com:2181,h12.bdiiot.com:2181,h13.bdiiot.com:2181 \
---add \
+--remove \
 --allow-principal User:bigdata \
 --consumer \
 --topic ${topic} \
---group ${group}
+--group \*
+else
+
+/usr/hdp/current/kafka-broker/bin/kafka-acls.sh \
+--authorizer-properties zookeeper.connect=h11.bdiiot.com:2181,h12.bdiiot.com:2181,h13.bdiiot.com:2181 \
+--remove \
+--allow-principal User:bigdata \
+--consumer \
+--topic ${topic} \
+--group test_console-consumer
+fi
