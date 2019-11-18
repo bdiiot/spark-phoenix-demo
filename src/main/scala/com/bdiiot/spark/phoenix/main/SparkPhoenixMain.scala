652ce02b301d75ffea1570f437967fb4db3f1334
Eber H <eber.h@outlook.com>
2019-11-18 19:29:00 +0800

    phoenix jdbc

--- src/main/scala/com/bdiiot/spark/phoenix/main/SparkPhoenixMain.scala
+++ src/main/scala/com/bdiiot/spark/phoenix/main/SparkPhoenixMain.scala
@@ -1,8 +1,11 @@
 package com.bdiiot.spark.phoenix.main
 
+import java.sql.{Connection, DriverManager, ResultSet, Statement}
+
 import com.bdiiot.spark.phoenix.utils.Constant._
 import com.bdiiot.spark.phoenix.utils.SparkHelper
 import org.apache.spark.sql
+import org.apache.spark.sql.ForeachWriter
 
 object SparkPhoenixMain {
   def main(args: Array[String]): Unit = {
@@ -25,25 +28,41 @@ object SparkPhoenixMain {
     import spark.implicits._
     val kafkaSourceString = kafkaSource.selectExpr("CAST(value AS STRING)").as[String]
 
-    // output to console
-    val query = kafkaSourceString
-      .writeStream
-      .format(CONSOLE_SOURCE)
+    val query = kafkaSourceString.writeStream
+      .foreach(new ForeachWriter[String] {
+        var connection: Connection = _
+        var statement: Statement = _
+        var resultSet: ResultSet = _
+
+        override def open(partitionId: Long, version: Long): Boolean = {
+          val jdbc_url = "jdbc:phoenix:h11.bdiiot.com,h12.bdiiot.com,h13.bdiiot.com:2181:/hbase-secure:hbase-bdiiot@BDIIOT.COM:/etc/security/keytabs/hbase.headless.keytab"
+          Class.forName("org.apache.phoenix.jdbc.PhoenixDriver")
+          connection = DriverManager.getConnection(jdbc_url)
+          statement = connection.createStatement
+          true
+        }
+
+        override def process(value: String): Unit = {
+          var sql = "upsert into test.test_phoenix values (1,'a')"
+          statement.executeUpdate(sql)
+          connection.commit()
+
+          sql = "select * from test.test_phoenix"
+          val resultSet = statement.executeQuery(sql)
+          while (resultSet.next()) {
+            println(resultSet.getObject(1).toString)
+          }
+        }
+
+        override def close(errorOrNull: Throwable): Unit = {
+          resultSet.close()
+          statement.close()
+          connection.close()
+        }
+      })
       .outputMode(OUTPUT_MODE)
       .option("checkpointLocation", PATH_CHECKPOINT + "demo")
       .start()
-
-    //    val query = kafkaSourceString.writeStream
-    //      .foreach(new ForeachWriter[String] {
-    //        override def open(partitionId: Long, version: Long): Boolean = ???
-    //
-    //        override def process(value: String): Unit = ???
-    //
-    //        override def close(errorOrNull: Throwable): Unit = ???
-    //      })
-    //      .outputMode(OUTPUT_MODE)
-    //      .option("checkpointLocation", PATH_CHECKPOINT + "spark_phoenix_demo")
-    //      .start()
 
     query.awaitTermination()
     SparkHelper.close
