22a0cb15327d40c699f5a1c39a635fe8c53442f2
Eber H <eber.h@outlook.com>
2019-11-19 12:26:55 +0800

    URLClassLoader

--- src/main/scala/com/bdiiot/spark/phoenix/main/SparkPhoenixMain.scala
+++ src/main/scala/com/bdiiot/spark/phoenix/main/SparkPhoenixMain.scala
@@ -1,11 +1,15 @@
 package com.bdiiot.spark.phoenix.main
 
+import java.net.URL
 import java.sql.{Connection, DriverManager, ResultSet, Statement}
 
 import com.bdiiot.spark.phoenix.utils.Constant._
 import com.bdiiot.spark.phoenix.utils.SparkHelper
 import org.apache.spark.sql
 import org.apache.spark.sql.ForeachWriter
+
+import scala.reflect.internal.util.ScalaClassLoader.URLClassLoader
+
 
 object SparkPhoenixMain {
   def main(args: Array[String]): Unit = {
@@ -35,6 +39,8 @@ object SparkPhoenixMain {
         var resultSet: ResultSet = _
 
         override def open(partitionId: Long, version: Long): Boolean = {
+          val url = "/usr/hdp/current/phoenix-client/phoenix-client.jar"
+          new URLClassLoader(Seq(new URL(url)), Thread.currentThread().getContextClassLoader())
           val jdbc_url = "jdbc:phoenix:h11.bdiiot.com,h12.bdiiot.com,h13.bdiiot.com:2181:/hbase-secure:hbase-bdiiot@BDIIOT.COM:/etc/security/keytabs/hbase.headless.keytab"
           Class.forName("org.apache.phoenix.jdbc.PhoenixDriver")
           connection = DriverManager.getConnection(jdbc_url)
