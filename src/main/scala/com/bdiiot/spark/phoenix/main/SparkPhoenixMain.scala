58c2ddb9b9108823a0f2e3ca1a4cbb2b2dead84c
Eber H <eber.h@outlook.com>
2019-11-20 12:24:43 +0800

    try catch

--- src/main/scala/com/bdiiot/spark/phoenix/main/SparkPhoenixMain.scala
+++ src/main/scala/com/bdiiot/spark/phoenix/main/SparkPhoenixMain.scala
@@ -4,10 +4,6 @@ import java.sql.{Connection, DriverManager, ResultSet, Statement}
 
 import com.bdiiot.spark.phoenix.utils.Constant._
 import com.bdiiot.spark.phoenix.utils.SparkHelper
-import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.Path
-import org.apache.hadoop.hbase.HBaseConfiguration
-import org.apache.hadoop.security.UserGroupInformation
 import org.apache.spark.sql
 import org.apache.spark.sql.ForeachWriter
 
@@ -40,16 +36,16 @@ object SparkPhoenixMain {
         var resultSet: ResultSet = _
 
         override def open(partitionId: Long, version: Long): Boolean = {
-          val config: Configuration = HBaseConfiguration.create()
-          config.addResource(new Path(CORE_SITE))
-          config.addResource(new Path(HDFS_SITE))
-          config.addResource(new Path(HBASE_SITE))
-
-          val configuration: Configuration = new Configuration()
-          configuration.set("hadoop.security.authentication", "Kerberos")
-
-          UserGroupInformation.setConfiguration(configuration)
-          UserGroupInformation.loginUserFromKeytab(USER, KEYTAB)
+          //          val config: Configuration = HBaseConfiguration.create()
+          //          config.addResource(new Path(CORE_SITE))
+          //          config.addResource(new Path(HDFS_SITE))
+          //          config.addResource(new Path(HBASE_SITE))
+          //
+          //          val configuration: Configuration = new Configuration()
+          //          configuration.set("hadoop.security.authentication", "Kerberos")
+          //
+          //          UserGroupInformation.setConfiguration(configuration)
+          //          UserGroupInformation.loginUserFromKeytab(USER, KEYTAB)
 
           val jdbc_url = "jdbc:phoenix:h11.bdiiot.com,h12.bdiiot.com,h13.bdiiot.com:2181:/hbase-secure:hbase-bdiiot@BDIIOT.COM:/etc/security/keytabs/hbase.headless.keytab"
           Class.forName("org.apache.phoenix.jdbc.PhoenixDriver")
@@ -71,9 +67,27 @@ object SparkPhoenixMain {
         }
 
         override def close(errorOrNull: Throwable): Unit = {
-          resultSet.close()
-          statement.close()
-          connection.close()
+          try {
+            resultSet.close()
+          } catch {
+            case ex: Exception => {
+              println(s"close resultSet failed, msg=$ex")
+            }
+          }
+          try {
+            statement.close()
+          } catch {
+            case ex: Exception => {
+              println(s"close statement failed, msg=$ex")
+            }
+          }
+          try {
+            connection.close()
+          } catch {
+            case ex: Exception => {
+              println(s"close connection failed, msg=$ex")
+            }
+          }
         }
       })
       .outputMode(OUTPUT_MODE)
