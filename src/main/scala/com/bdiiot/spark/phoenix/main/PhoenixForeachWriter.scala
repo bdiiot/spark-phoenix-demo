8b3a199a9d20c055584165fa827eb40279e9e4a3
Eber H <eber.h@outlook.com>
2019-11-20 15:09:10 +0800

    PhoenixForeachWriter

---
+++ src/main/scala/com/bdiiot/spark/phoenix/main/PhoenixForeachWriter.scala
@@ -0,0 +1,59 @@
+package com.bdiiot.spark.phoenix.main
+
+import java.sql.{Connection, DriverManager, ResultSet, Statement}
+
+import com.bdiiot.spark.phoenix.utils.Constant.{JDBC_URL, PHOENIX_DRIVER}
+import org.apache.spark.sql.ForeachWriter
+
+object PhoenixForeachWriter {
+  def apply(): ForeachWriter[String] = {
+    new PhoenixForeachWriter()
+  }
+}
+
+class PhoenixForeachWriter() extends ForeachWriter[String] {
+  var connection: Connection = _
+  var statement: Statement = _
+  var resultSet: ResultSet = _
+
+  override def open(partitionId: Long, version: Long): Boolean = {
+    Class.forName(PHOENIX_DRIVER)
+    connection = DriverManager.getConnection(JDBC_URL)
+    statement = connection.createStatement
+    true
+  }
+
+  override def process(value: String): Unit = {
+    val key = System.nanoTime().toString.reverse
+    var sql = s"upsert into test.test_phoenix values ($key,'$value')"
+    statement.executeUpdate(sql)
+    connection.commit()
+
+    sql = "select * from test.test_phoenix"
+    val resultSet = statement.executeQuery(sql)
+    while (resultSet.next()) {
+      println(resultSet.getObject(2).toString)
+    }
+  }
+
+  override def close(errorOrNull: Throwable): Unit = {
+    try {
+      resultSet.close()
+    } catch {
+      case ex: Exception =>
+        println(s"close resultSet failed, msg=$ex")
+    }
+    try {
+      statement.close()
+    } catch {
+      case ex: Exception =>
+        println(s"close statement failed, msg=$ex")
+    }
+    try {
+      connection.close()
+    } catch {
+      case ex: Exception =>
+        println(s"close connection failed, msg=$ex")
+    }
+  }
+}
--- src/main/scala/com/bdiiot/spark/phoenix/main/SparkPhoenixMain.scala
+++ src/main/scala/com/bdiiot/spark/phoenix/main/SparkPhoenixMain.scala
@@ -1,11 +1,8 @@
 package com.bdiiot.spark.phoenix.main
 
-import java.sql.{Connection, DriverManager, ResultSet, Statement}
-
 import com.bdiiot.spark.phoenix.utils.Constant._
 import com.bdiiot.spark.phoenix.utils.SparkHelper
 import org.apache.spark.sql
-import org.apache.spark.sql.ForeachWriter
 
 
 object SparkPhoenixMain {
@@ -30,55 +27,7 @@ object SparkPhoenixMain {
     val kafkaSourceString = kafkaSource.selectExpr("CAST(value AS STRING)").as[String]
 
     val query = kafkaSourceString.writeStream
-      .foreach(new ForeachWriter[String] {
-        var connection: Connection = _
-        var statement: Statement = _
-        var resultSet: ResultSet = _
-
-        override def open(partitionId: Long, version: Long): Boolean = {
-          Class.forName(PHOENIX_DRIVER)
-          connection = DriverManager.getConnection(JDBC_URL)
-          statement = connection.createStatement
-          true
-        }
-
-        override def process(value: String): Unit = {
-          val key = System.nanoTime().toString.reverse
-          var sql = s"upsert into test.test_phoenix values (${key},'${value}')"
-          statement.executeUpdate(sql)
-          connection.commit()
-
-          sql = "select * from test.test_phoenix"
-          val resultSet = statement.executeQuery(sql)
-          while (resultSet.next()) {
-            println(resultSet.getObject(2).toString)
-          }
-        }
-
-        override def close(errorOrNull: Throwable): Unit = {
-          try {
-            resultSet.close()
-          } catch {
-            case ex: Exception => {
-              println(s"close resultSet failed, msg=$ex")
-            }
-          }
-          try {
-            statement.close()
-          } catch {
-            case ex: Exception => {
-              println(s"close statement failed, msg=$ex")
-            }
-          }
-          try {
-            connection.close()
-          } catch {
-            case ex: Exception => {
-              println(s"close connection failed, msg=$ex")
-            }
-          }
-        }
-      })
+      .foreach(PhoenixForeachWriter.apply())
       .outputMode(OUTPUT_MODE)
       .option("checkpointLocation", PATH_CHECKPOINT + "demo")
       .start()
