85b076e96f7315b9f209478a984294f8fc953981
Eber H <eber.h@outlook.com>
2019-11-29 16:47:30 +0800

    maven-assembly-plugin

--- src/main/scripts/submit.sh
+++ src/main/scripts/submit.sh
@@ -10,7 +10,7 @@ export SPARK_MAJOR_VERSION=2
 spark-submit \
 --master yarn \
 --deploy-mode client \
---jars "/usr/hdp/current/phoenix-client/phoenix-client.jar" \
+--jars "spark-phoenix-demo-1.0-SNAPSHOT-jar-with-dependencies.jar" \
 --conf "spark.executor.extraJavaOptions=-Djava.security.auth.login.config=/tmp/kafka_bigdata_jaas.conf" \
 --class com.bdiiot.spark.phoenix.main.SparkPhoenixMain \
 spark-phoenix-demo-1.0-SNAPSHOT.jar
